f66c0b60c745639d731e0037fefca3bdd8d0a31f
SahilSadhwani <sadhwanisahil64@gmail.com>
2019-10-30 18:39:02 +0530

    updated index.html and other js files

--- src/js/app.js
+++ src/js/app.js
@@ -1,63 +1,125 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return await App.initWeb3();
+init: function() {
+    return App.initWeb3();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+initWeb3: function() {
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
+    
+  redirect: function(){
+    window.location.href = "http://localhost:3000/manufacturer/dashboard.html";  
+  },
+
+
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
+      alert("ss");
+//    $.getJSON("ApproveOrder.json", function(certificate) {
+//      // Instantiate a new truffle contract from the artifact
+//      App.contracts.ApproveOrder = TruffleContract(certificate);
+//      // Connect provider to interact with contract
+//      App.contracts.ApproveOrder.setProvider(App.web3Provider);
+//
+//      //App.listenForEvents();    
+//
+      return App.render();
+//    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+
+
+
+
+
+
+render: function() {
+//    var electionInstance;
+//    var loader = $("#loader");
+//    var content = $("#content");
+//
+//    loader.show();
+    //content.hide();
+    
+    
+    
+//    var sa = $("#warehouse");
+//    sa.click(function(){
+//       $("#warehouse").css("color","green"); 
+//    });
+    // Load account data
+    // web3.eth.getAccounts().then(function (result){
+    //   //return result[0]
+    //   App.account=result[0]
+    //   $("#accountAddress").html("Your Account: " + result[0]);
+    //   });
+    
+
+      //web3.eth.getA
+   
+    
+    try{
+         web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        //var account = web3.eth.accounts[1];
+        App.account = account;
+          alert(account);
+        //web3.eth.getAccounts(accounts => console.log(accounts[0])).
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    }).catch(function(error) {
+      console.warn(error);
+    });
+    }finally{
+//        return App.addO();   
+    }
+    
+    
+    
   },
+    
+    addO: function(){
+        var url=$(location).attr("href");
+    var urlarray=url.split("?");
+    var splitmeagain=urlarray[1];
+    var semifinalsplit=splitmeagain.split("&");
+    var finalsplit=semifinalsplit[1].split("=");
+    var number=finalsplit[1];
+    var orderid=semifinalsplit[0].split("=");
+    orderid=orderid[1];
+    var quantity=semifinalsplit[1].split("=");
+    quantity=quantity[1];
+    if(orderid>0 && quantity>0){
+        alert(">");
+        
+        App.contracts.ApproveOrder.deployed().then(function(instance){
+            return instance.addOrder(orderid, 1, 4, quantity, "sasbsdfaj362neksj3bsbsafg", {from: App.account, gas:300000}).then(function(result){
+                
+            }).catch(function(err){
+               console.log(err+"sassasasasasa"); 
+            });
+        });
+    }
+    },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+ 
 
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
 
 };
 
--- src/js/main.js
+++ src/js/main.js
@@ -64,96 +64,96 @@ jQuery(document).on('ready', function ($) {
     /*---------------------------
         HOME SLIDER
     -----------------------------*/
-    var $homeSlider = $('.welcome-slider-area');
-    $homeSlider.owlCarousel({
-        merge: true,
-        smartSpeed: 2000,
-        loop: true,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        autoplay: true,
-        autoplayTimeout: 5000,
-        margin: 0,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            },
-            1200: {
-                items: 1
-            }
-        }
-    });
+//    var $homeSlider = $('.welcome-slider-area');
+//    $homeSlider.owlCarousel({
+//        merge: true,
+//        smartSpeed: 2000,
+//        loop: true,
+//        nav: true,
+//        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+//        autoplay: true,
+//        autoplayTimeout: 5000,
+//        margin: 0,
+//        animateIn: 'fadeIn',
+//        animateOut: 'fadeOut',
+//        responsiveClass: true,
+//        responsive: {
+//            0: {
+//                items: 1
+//            },
+//            600: {
+//                items: 1
+//            },
+//            1000: {
+//                items: 1
+//            },
+//            1200: {
+//                items: 1
+//            }
+//        }
+//    });
 
 
     /*------------------------------
         TESTMONIAL SLIDER
     -------------------------------*/
-    var client_photo2 = $('.client_details');
-    client_photo2.owlCarousel({
-        loop: true,
-        margin: 30,
-        autoplay: false,
-        dots: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 600,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            992: {
-                items: 1
-            }
-        }
-    });
+//    var client_photo2 = $('.client_details');
+//    client_photo2.owlCarousel({
+//        loop: true,
+//        margin: 30,
+//        autoplay: false,
+//        dots: true,
+//        autoplayTimeout: 4000,
+//        smartSpeed: 600,
+//        responsive: {
+//            0: {
+//                items: 1
+//            },
+//            600: {
+//                items: 1
+//            },
+//            992: {
+//                items: 1
+//            }
+//        }
+//    });
     
-    var client_photo = $('.client_photo');
-    client_photo.owlCarousel({
-        loop: true,
-        center: true,
-        margin: 0,
-        dots: true,
-        autoplayTimeout: 4000,
-        smartSpeed: 600,
-        mouseDrag: true,
-        touchDrag: false,
-        center: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            992: {
-                items: 3
-            }
-        }
-    });
-    $('.client_nav .testi_next').on('click', function () {
-        client_photo.trigger('next.owl.carousel');
-    });
-    $('.client_nav .testi_prev').on('click', function () {
-        client_photo.trigger('prev.owl.carousel');
-    });
-
-    client_photo.on('translate.owl.carousel', function (property) {
-        $('.client-details-content .owl-dot:eq(' + property.page.index + ')').click();
-    });
-    client_photo2.on('translate.owl.carousel', function (property) {
-        $('.client-photo-list .owl-dot:eq(' + property.page.index + ')').click();
-    });
+//    var client_photo = $('.client_photo');
+//    client_photo.owlCarousel({
+//        loop: true,
+//        center: true,
+//        margin: 0,
+//        dots: true,
+//        autoplayTimeout: 4000,
+//        smartSpeed: 600,
+//        mouseDrag: true,
+//        touchDrag: false,
+//        center: true,
+//        responsive: {
+//            0: {
+//                items: 1
+//            },
+//            600: {
+//                items: 3
+//            },
+//            992: {
+//                items: 3
+//            }
+//        }
+//    });
+//    $('.client_nav .testi_next').on('click', function () {
+//        client_photo.trigger('next.owl.carousel');
+//    });
+//    $('.client_nav .testi_prev').on('click', function () {
+//        client_photo.trigger('prev.owl.carousel');
+//    });
+//
+//    client_photo.on('translate.owl.carousel', function (property) {
+//        $('.client-details-content .owl-dot:eq(' + property.page.index + ')').click();
+//    });
+//    client_photo2.on('translate.owl.carousel', function (property) {
+//        $('.client-photo-list .owl-dot:eq(' + property.page.index + ')').click();
+//    });
 
     /*----------------------------
         TAB PANEL ACTIVE
@@ -171,28 +171,28 @@ jQuery(document).on('ready', function ($) {
     /*---------------------------
         TESTMONIAL SLIDER
     -----------------------------*/
-    $('.client-list').owlCarousel({
-        merge: true,
-        smartSpeed: 1000,
-        loop: true,
-        nav: false,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        autoplay: false,
-        autoplayTimeout: 2000,
-        margin: 10,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 2
-            },
-            600: {
-                items: 4
-            },
-            1000: {
-                items: 6
-            }
-        }
-    });
+//    $('.client-list').owlCarousel({
+//        merge: true,
+//        smartSpeed: 1000,
+//        loop: true,
+//        nav: false,
+//        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+//        autoplay: false,
+//        autoplayTimeout: 2000,
+//        margin: 10,
+//        responsiveClass: true,
+//        responsive: {
+//            0: {
+//                items: 2
+//            },
+//            600: {
+//                items: 4
+//            },
+//            1000: {
+//                items: 6
+//            }
+//        }
+//    });
 
 
     /*--------------------------
